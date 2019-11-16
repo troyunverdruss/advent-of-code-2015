9d9ebe82ac49779162233bfaf054c4dd92f56c37
Troy Unverdruss <troy@unverdruss.net>
2019-11-16 13:46:50 -0800

    Getting some basics working and day 1 solved

--- src/day01.ts
+++ src/day01.ts
@@ -1,4 +1,23 @@
-function x() {
-   console.log("as");
-}
-x();
+import { loadInput } from './utils';
+
+const lines = loadInput(1);
+
+const data = lines[0];
+var floor = 0;
+var position = 0;
+var negativePosition: number = -1;
+
+data.split('').forEach(letter => {
+    position += 1;
+    if (letter === '(') {
+        floor += 1;
+    } else {
+        floor -= 1;
+    }
+    if (floor < 0 && negativePosition == -1) {
+        negativePosition = position;
+    }
+})
+
+console.log('Part 1: ' + floor);
+console.log('Part 2: ' + negativePosition)
