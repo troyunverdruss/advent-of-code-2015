0e662ab9ed08266d75e1ee91035c2d1d6014a8e1
Troy Unverdruss <troy@scroll.com>
2019-11-16 20:28:35 -0800

    day 3 complete

---
+++ test/day03.test.ts
@@ -0,0 +1,42 @@
+import { Point, part1 } from "../src/day03";
+
+test("points equal", () => {
+  const a = new Point(1, 2);
+  const b = new Point(1, 2);
+
+  expect(a).toEqual(b);
+});
+
+test("example 1", () => {
+  const data = ">";
+  const count = part1(data);
+  expect(count).toBe(2);
+});
+
+test("example 2", () => {
+  const data = "^>v<";
+  const count = part1(data);
+  expect(count).toBe(4);
+});
+
+test("example 3", () => {
+  const data = "^v^v^v^v^v";
+  const count = part1(data);
+  expect(count).toBe(2);
+});
+
+test("sets work?", () => {
+  const s = new Set();
+  s.add(1);
+  s.add(1);
+  s.add(2);
+  expect(s.size).toBe(2);
+});
+
+test("sets work with points?", () => {
+  const s = new Set();
+  s.add(new Point(1, 2).toString());
+  s.add(new Point(1, 2).toString());
+  s.add(new Point(2, 2).toString());
+  expect(s.size).toBe(2);
+});
