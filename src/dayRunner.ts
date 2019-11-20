f8648e8c2997e1e0d61b7b928f9145dc065a94a8
Troy Unverdruss <troy@scroll.com>
2019-11-19 17:50:20 -0800

    using import instead of require

--- src/dayRunner.ts
+++ src/dayRunner.ts
@@ -1,4 +1,4 @@
-let utils = require('./utils');
+import { formatDay } from "./utils";
 
 // Take an argument, pad it if necessary, then try to run the file
-require("./day" + utils.formatDay(process.argv[2]));
+require("./day" + formatDay(Number.parseInt(process.argv[2])));
