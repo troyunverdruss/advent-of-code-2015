975b9b46fae30a75207dc878b4f197444bd5247c
Troy Unverdruss <troy@scroll.com>
2019-11-25 09:22:57 -0800

    day 12

---
+++ src/day12.ts
@@ -0,0 +1,32 @@
+import { loadInput } from "./utils";
+
+function findNumbers(results: number[], arg: any, ignoreRed: boolean) {
+  if (typeof arg === "number") {
+    results.push(arg);
+  } else if (arg instanceof Array) {
+    arg.map(v => findNumbers(results, v, ignoreRed));
+  } else if (arg instanceof Object) {
+    if (ignoreRed) {
+      const maybeRed = Object.values(arg).filter(v => v === "red");
+
+      if (maybeRed.length === 0) {
+        Object.keys(arg).map(k => findNumbers(results, arg[k], ignoreRed));
+      }
+    } else {
+      Object.keys(arg).map(k => findNumbers(results, arg[k], ignoreRed));
+    }
+  }
+}
+
+const rawData = loadInput(12)[0];
+const js = JSON.parse(rawData);
+
+const part1Results = [];
+findNumbers(part1Results, js, false);
+const sum1 = part1Results.reduce((a, b) => a + b);
+console.log(`Part 1: ${sum1}`);
+
+const part2Results = [];
+findNumbers(part2Results, js, true);
+const sum2 = part2Results.reduce((a, b) => a + b);
+console.log(`Part 2: ${sum2}`);
