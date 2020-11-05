86d6e6d4543db11a915745882cd8fce162127bb5
Troy Unverdruss <troy@scroll.com>
2020-11-04 16:50:18 -0800

    day 19 second star finally

--- src/day19.ts
+++ src/day19.ts
@@ -1,8 +1,10 @@
-import { loadInput } from "./utils";
+import {loadInput} from "./utils";
+import PriorityQueue from "ts-priority-queue";
+import {distance, closest} from 'fastest-levenshtein'
 
 export function part1(
-  molecule: string[],
-  conversions: Map<string, Array<string>>
+    molecule: string[],
+    conversions: Map<string, Array<string>>
 ): number {
   const combos = new Set();
   // console.log("conversions", conversions);
@@ -18,18 +20,230 @@ export function part1(
     });
   });
 
-  // console.log("combos", combos);
-  // let combos = 1;
-  // molecule.forEach((atom) => {
-  //   console.log("a", combos, atom);
-  //   const count = conversionCount.get(atom);
-
-  //   if (count !== undefined) {
-  //     combos *= count;
-  //   }
-  // });
-
   return combos.size;
+}
+
+/** Wish I'd tried this first but I just kinda half assumed it wouldn't yield
+ * the *shortest* path, but maybe I got lucky or maybe there's only actually
+ * one path. This way works backwards instead of forwards which makes it
+ * massively more efficient */
+export function part2(
+    molecule: string[],
+    conversions: Map<string, Array<string>>
+): number {
+  // Reverse the conversions map
+  const reverseConversions = new Map<string, string>();
+  conversions.forEach((values, key) => {
+    values.forEach((value) => reverseConversions.set(value, key))
+  })
+
+  let currentMolecule = molecule.join('')
+  let steps = 0
+  while (currentMolecule !== 'e') {
+    for (const k of reverseConversions.keys()) {
+      if (currentMolecule.indexOf(k) >= 0) {
+        const replacement = reverseConversions.get(k)
+        if (!replacement) {
+          throw Error(`replacement is missing for key ${k}!!`)
+        }
+
+        if (replacement === 'e' && k.length !== currentMolecule.length) {
+          continue
+        }
+        currentMolecule = currentMolecule.replace(k, replacement);
+        steps += 1
+      }
+    }
+  }
+
+  return steps
+}
+
+
+/** This actually works, but it's way too slow for the task at hand
+ * No idea where it runs out of memory, but it always does */
+export function part2SearchBased(
+    molecule: string[],
+    conversions: Map<string, Array<string>>
+): number {
+
+  const targetMolecule = molecule.join('')
+  const targetMoleculeLength = molecule.length
+
+  const fitnessFunc = (a: StateWithScore, b: StateWithScore): number => {
+    return a.score - b.score
+  }
+
+  const open = new PriorityQueue({comparator: fitnessFunc});
+  const closed = new Map();
+  const openSet = new Set();
+
+  open.queue({
+    score: 1,
+    state: ["e"],
+    steps: 0
+  })
+  openSet.add('e')
+
+  let found = false;
+  let foundSteps = 1000000;
+
+  while (open.length > 0 && !found) {
+
+    console.log(`open: ${open.length}`)
+    console.log(`openSet: ${openSet.size}`)
+    console.log(`closed: ${closed.size}`)
+
+    const states = findValidNextStates(conversions, open.peek().state);
+
+    let state = open.dequeue();
+    openSet.delete(state.state.join(''))
+    // console.log(state.state)
+    console.log(`length: ${state.state.length}`)
+
+    closed.set(state.state.join(''), state.steps);
+
+
+    states
+    .filter(s => {
+      if (!closed.has(s)) {
+        return true
+      }
+
+      if (closed.has(s)) {
+        if (closed.get(s) > state.steps) {
+          return true
+        }
+      }
+
+      return false;
+    })
+    .filter(() => state.steps + 1 <= foundSteps)
+    .filter(s => parseMoleculeString(s).length <= targetMoleculeLength)
+    .forEach(s => {
+      // if (state.steps + 1 === 5) {
+      //   console.log(`steps 5, state: ${s}`)
+      // }
+      if (s === targetMolecule) {
+        found = true;
+        // console.log(`Found ${s} with previous state: ${state.state}, steps: ${state.steps}`);
+        if (state.steps + 1 < foundSteps) {
+          foundSteps = state.steps + 1;
+        }
+      }
+
+      if (!openSet.has(s) && !closed.has(s)) {
+        open.queue({
+          score: distance(s, targetMolecule),
+          state: parseMoleculeString(s),
+          steps: state.steps + 1
+        });
+        openSet.add(s)
+      }
+    })
+
+  }
+
+  return foundSteps;
+}
+
+export function levenshtein(source: string[], target: string[]): number {
+  const sizeX = source.length + 1
+  const sizeY = target.length + 1
+
+  let cost: number[] = []
+  let newCost: number[] = []
+
+  for (let i = 0; i < sizeX; i++) {
+    cost[i] = i
+  }
+
+  for (let j = 1; j < sizeY; j++) {
+    newCost[0] = j
+    for (let i = 1; i < sizeX; i++) {
+      const match = source[i - 1] === target[j - 1] ? 0 : 1
+
+      const costReplace = cost[i - 1] + match;
+      const costInsert = cost[i] + 1;
+      const costDelete = newCost[i - 1] + 1;
+
+      newCost[i] = Math.min(costReplace, costInsert, costDelete)
+    }
+    const swap = cost;
+    cost = newCost
+    newCost = swap
+  }
+
+  return cost[sizeX - 1]
+}
+
+export function levenshteinSlow(source: string, target: string): number {
+  const grid = new Map();
+  const sizeX = source.length + 1
+  const sizeY = target.length + 1
+
+  for (let x = 0; x < sizeX; x++) {
+    for (let y = 0; y < sizeY; y++) {
+      grid.set(xy(x, y), 0)
+    }
+  }
+
+  for (let x = 0; x < sizeX; x++) {
+    grid.set(xy(x, 0), x)
+  }
+
+  for (let y = 0; y < sizeY; y++) {
+    grid.set(xy(0, y), y)
+  }
+
+  for (let x = 1; x < sizeX; x++) {
+    for (let y = 1; y < sizeY; y++) {
+      if (source[x - 1] === target[y - 1]) {
+        const cost = Math.min(
+            grid.get(xy(x - 1, y)) + 1,
+            grid.get(xy(x - 1, y - 1)),
+            grid.get(xy(x, y - 1)) + 1
+        )
+        grid.set(xy(x, y), cost)
+      } else {
+        const cost = Math.min(
+            grid.get(xy(x - 1, y)) + 1,
+            grid.get(xy(x - 1, y - 1)) + 1,
+            grid.get(xy(x, y - 1)) + 1
+        )
+        grid.set(xy(x, y), cost)
+      }
+    }
+  }
+
+  return grid.get(xy(sizeX - 1, sizeY - 1))
+}
+
+function xy(x: number, y: number): string {
+  return `${x} ${y}`
+}
+
+export function findValidNextStates(conversions: Map<string, Array<string>>, state: string[]): string[] {
+  let nextStates: string[] = []
+  for (let i = 0; i < state.length; i++) {
+    const start = state.slice(0, i).join('');
+    const sub = state[i];
+    const end = state.slice(i + 1).join('')
+
+    const replacements: string[] = conversions.get(sub) ?? []
+    replacements.forEach(r => {
+      if (!nextStates.includes(start + r + end)) {
+        nextStates.push(start + r + end);
+      }
+    })
+  }
+  return nextStates;
+}
+
+interface StateWithScore {
+  score: number,
+  state: string[],
+  steps: number
 }
 
 interface ParseResult {
@@ -43,25 +257,31 @@ export function parseInput(lines: string[]): ParseResult {
 
   // console.log(moleculeStr);
 
-  const molecule = moleculeStr
-    .split(/([A-Z]{1}[a-z]*)/g)
-    .filter((s) => s !== "");
+  const molecule = parseMoleculeString(moleculeStr)
 
   for (let i = 0; i < lines.length - 1; i++) {
     const tokens = lines[i].split(" => ");
 
     if (!conversions.get(tokens[0])) {
-      conversions.set(tokens[0], new Array());
+      conversions.set((tokens[0]), []);
     }
-    conversions.get(tokens[0])?.push(tokens[1]);
+    conversions.get((tokens[0]))?.push((tokens[1]));
   }
-  return { conversions: conversions, molecule: molecule };
+  return {conversions: conversions, molecule: molecule};
+}
+
+export function parseMoleculeString(moleculeStr: string): string[] {
+  return moleculeStr
+  .split(/([A-Z][a-z]*)/g)
+  .filter((s) => s !== "");
 }
 
 if (require.main === module) {
   const lines = loadInput(19).filter((l) => l != "");
-  const { conversions, molecule } = parseInput(lines);
+  const {conversions, molecule} = parseInput(lines);
   // console.log(molecule);
   // console.log(conversionCount);
   console.log(`Part 1: ${part1(molecule, conversions)}`);
+
+  console.log(`Part 2: ${part2(molecule, conversions)}`)
 }
--- test/day19.test.ts
+++ test/day19.test.ts
@@ -1,48 +1,146 @@
-import { part1, parseInput } from "../src/day19";
+import {
+  part1,
+  parseInput,
+  part2,
+  findValidNextStates,
+  parseMoleculeString,
+  levenshtein, Replacer
+} from '../src/day19';
 
-const lines = ["H => HO", "H => OH", "O => HH", "HOH"];
+const lines = ['H => HO', 'H => OH', 'O => HH', 'HOH'];
+const part2Lines = [
+  'e => H',
+  'e => O',
+  'H => HO',
+  'H => OH',
+  'O => HH'
 
-test("parsing the input", () => {
-  const { conversions, molecule } = parseInput(lines);
+]
+
+test('parsing the input', () => {
+  const {conversions, molecule} = parseInput(lines);
 
   expect(conversions.size).toBe(2);
 
-  expect(molecule[0]).toBe("H");
-  expect(molecule[1]).toBe("O");
-  expect(molecule[2]).toBe("H");
-  expect(molecule).toStrictEqual(["H", "O", "H"]);
+  expect(molecule[0]).toBe('H');
+  expect(molecule[1]).toBe('O');
+  expect(molecule[2]).toBe('H');
+  expect(molecule).toStrictEqual(['H', 'O', 'H']);
 
-  expect(conversions.get("H")).toStrictEqual(["HO", "OH"]);
-  expect(conversions.get("O")).toStrictEqual(["HH"]);
+  expect(conversions.get('H')).toStrictEqual(['HO', 'OH']);
+  expect(conversions.get('O')).toStrictEqual(['HH']);
 });
 
-test("parsing more complicated molecule", () => {
-  const { conversions, molecule } = parseInput(["Al => H", "H => OH", "HAlH"]);
+test('parsing more complicated molecule', () => {
+  const {conversions, molecule} = parseInput(['Al => H', 'H => OH', 'HAlH']);
 
   expect(conversions.size).toBe(2);
 
-  expect(molecule[0]).toBe("H");
-  expect(molecule[1]).toBe("Al");
-  expect(molecule[2]).toBe("H");
-  expect(molecule).toStrictEqual(["H", "Al", "H"]);
+  expect(molecule[0]).toBe('H');
+  expect(molecule[1]).toBe('Al');
+  expect(molecule[2]).toBe('H');
+  expect(molecule).toStrictEqual(['H', 'Al', 'H']);
 
-  expect(conversions.get("H")).toStrictEqual(["OH"]);
-  expect(conversions.get("Al")).toStrictEqual(["H"]);
+  expect(conversions.get('H')).toStrictEqual(['OH']);
+  expect(conversions.get('Al')).toStrictEqual(['H']);
 });
 
-test("test the part 1 example", () => {
-  const { conversions, molecule } = parseInput(lines);
+test('test the part 1 example', () => {
+  const {conversions, molecule} = parseInput(lines);
 
   const result = part1(molecule, conversions);
 
   expect(result).toBe(4);
 });
 
-// test("test the part 2 example", () => {
-//   const lights = parseInput(lines);
-//   enableCorners(lights);
-//   // console.log(lights);
-//   animate(lights, 5, true);
-//   // console.log("lights", lights);
-//   expect(countLightsThatAreOn(lights)).toBe(17);
-// });
+test('test the part 2 example - 1', () => {
+  const {conversions, molecule} = parseInput(part2Lines.concat(['HOH']));
+
+  const result = part2(molecule, conversions);
+
+  expect(result).toBe(3);
+});
+
+test('test the part 2 example - 2', () => {
+  const {conversions, molecule} = parseInput(part2Lines.concat(['HOHOHO']));
+
+  const result = part2(molecule, conversions);
+
+  expect(result).toBe(6);
+});
+
+test('find next states from e', () => {
+  const {conversions} = parseInput(part2Lines);
+
+  const states = findValidNextStates(conversions, parseMoleculeString('e'))
+  console.log(states)
+  expect(states).toStrictEqual(['H', 'O'])
+
+})
+
+test('find next states from H', () => {
+  const {conversions} = parseInput(part2Lines);
+
+  const states = findValidNextStates(conversions, parseMoleculeString('H'))
+  console.log(states)
+  expect(states).toStrictEqual(['HO', 'OH'])
+
+})
+
+test('find next states from O', () => {
+  const {conversions} = parseInput(part2Lines.concat([""]));
+
+  const states = findValidNextStates(conversions, parseMoleculeString('O'))
+  console.log(states)
+  expect(states).toStrictEqual(['HH'])
+
+})
+
+test('find next states from HO', () => {
+  const {conversions} = parseInput(part2Lines.concat([""]));
+
+  const states = findValidNextStates(conversions, parseMoleculeString('HO'))
+  console.log(states)
+  expect(states).toStrictEqual(['HOO', 'OHO', 'HHH'])
+
+})
+
+test('find next states from OH', () => {
+  const {conversions} = parseInput(part2Lines.concat([""]));
+
+  const states = findValidNextStates(conversions, parseMoleculeString('OH'))
+  console.log(states)
+  expect(states).toStrictEqual(['HHH', 'OHO', 'OOH'])
+
+})
+
+
+test('find next states from HH', () => {
+  const {conversions} = parseInput(part2Lines);
+
+  const states = findValidNextStates(conversions, parseMoleculeString('HH'))
+  console.log(states)
+  expect(states).toStrictEqual(['HOH', 'OHH', 'HHO'])
+
+})
+
+test('lev 1', () => {
+  const d = levenshtein('test'.split(''), 'text'.split(''))
+  expect(d).toBe(1)
+})
+
+test('lev 2', () => {
+  const d = levenshtein('bashful'.split(''), 'happy'.split(''))
+  expect(d).toBe(6)
+})
+
+test('test replacer', () => {
+  const replacer = new Replacer()
+  const e = replacer.getReplacement('e')
+  const h = replacer.getReplacement('h')
+  const o = replacer.getReplacement('o')
+
+  expect(replacer.getReplacement('e')).toBe(e)
+  expect(replacer.getReplacement('h')).toBe(h)
+  expect(replacer.getReplacement('o')).toBe(o)
+})
